66225165eb32a6bd7739a3136536641d01d24441
lucianolucas <luciano.lucas@bllc.com.br>
2025-05-23 02:56:37 -0300

    10.13. Implementando endpoints do Sensor Monitoring

---
+++ src/main/java/com/algaworks/algasensors/temperature/monitoring/api/controller/SensorMonitoringController.java
@@ -0,0 +1,57 @@
+package com.algaworks.algasensors.temperature.monitoring.api.controller;
+
+import com.algaworks.algasensors.temperature.monitoring.api.model.SensorMonitoringOutput;
+import com.algaworks.algasensors.temperature.monitoring.domain.model.SensorId;
+import com.algaworks.algasensors.temperature.monitoring.domain.model.SensorMonitoring;
+import com.algaworks.algasensors.temperature.monitoring.domain.repository.SensorMonitoringRepository;
+import io.hypersistence.tsid.TSID;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/sensors/{sensorId}/monitoring")
+@RequiredArgsConstructor
+public class SensorMonitoringController {
+
+    private final SensorMonitoringRepository sensorMonitoringRepository;
+
+    @GetMapping
+    public SensorMonitoringOutput getDetail(@PathVariable TSID sensorId) {
+        SensorMonitoring sensorMonitoring = findByIdOrDefaultConfig(sensorId);
+        return SensorMonitoringOutput.builder()
+                .id(sensorMonitoring.getId().getValue())
+                .lastTemperature(sensorMonitoring.getLastTemperature())
+                .enabled(sensorMonitoring.getEnabled())
+                .updatedAt(sensorMonitoring.getUpdatedAt())
+                .build();
+    }
+
+    private SensorMonitoring findByIdOrDefaultConfig(TSID sensorId) {
+        return sensorMonitoringRepository.findById(new SensorId(sensorId))
+                .orElse(SensorMonitoring.builder()
+                        .id(new SensorId(sensorId))
+                        .enabled(false)
+                        .lastTemperature(null)
+                        .updatedAt(null)
+                        .build());
+    }
+
+    @PutMapping("/enable")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void enable (@PathVariable TSID sensorId) {
+       var sensor = findByIdOrDefaultConfig(sensorId);
+        sensor.setEnabled(true);
+        sensorMonitoringRepository.saveAndFlush(sensor);
+    }
+
+    @DeleteMapping("/enable")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void disable (@PathVariable TSID sensorId) {
+        var sensor = findByIdOrDefaultConfig(sensorId);
+        sensor.setEnabled(false);
+        sensorMonitoringRepository.saveAndFlush(sensor);
+    }
+
+
+}
