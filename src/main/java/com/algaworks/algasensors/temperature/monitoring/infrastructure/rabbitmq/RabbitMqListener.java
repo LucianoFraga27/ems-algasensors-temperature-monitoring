8faea5a47139849ad03de0690d18cadb33697d96
lucianolucas <luciano.lucas@bllc.com.br>
2025-06-08 21:20:32 -0300

    12.16. Competing Consumers Pattern

--- src/main/java/com/algaworks/algasensors/temperature/monitoring/infrastructure/rabbitmq/RabbitMqListener.java
+++ src/main/java/com/algaworks/algasensors/temperature/monitoring/infrastructure/rabbitmq/RabbitMqListener.java
@@ -22,12 +22,12 @@ public class RabbitMqListener {
 
     private final TemperatureMonitoringService temperatureMonitoringService;
 
-    @RabbitListener(queues = QUEUE)
+    @RabbitListener(queues = QUEUE, concurrency = "2-3")
     @SneakyThrows
     public void handle(@Payload TemperatureLogData temperatureLogData, @Headers Map<String, Object> headers){
-        var sendorId = temperatureLogData.getSensorId();
-        var temperature = temperatureLogData.getValue();
-        log.info("Temperature Listener: SensorId {} Temp {}",sendorId, temperature);
+//        var sendorId = temperatureLogData.getSensorId();
+//        var temperature = temperatureLogData.getValue();
+//        log.info("Temperature Listener: SensorId {} Temp {}",sendorId, temperature);
         temperatureMonitoringService.processTemperatureReading(temperatureLogData);
         Thread.sleep(Duration.ofSeconds(5));
 
